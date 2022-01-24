38c955640c9a84fa681cef2aff3e5b8dd14e7066
Zahid Hasan Milu <g.one.milu@gmail.com>
2022-01-25 00:12:48 +0600

    remove punctuations

---
+++ remove_punc_project/remove_punc_project/remove_punc_project/views.py
@@ -0,0 +1,22 @@
+from string import punctuation
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def analyze(request):
+    djtext = request.GET.get('text', 'default')
+    removepunc = request.GET.get('removepunc', 'off')
+    if removepunc == 'on':
+        punctuations = ''' !"#$%&'()*+, -./:;<=>?@[\]^_`{|}~'''
+        analyzed = ' '
+        for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed + char
+        param = {'purpose': 'remove punctuations', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', param)
+    else:
+        return HttpResponse("Errors")
