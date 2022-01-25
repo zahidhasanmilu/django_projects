08707d27cdc7348f0f42af50c8aba0072e9fc40f
Zahid Hasan Milu <g.one.milu@gmail.com>
2022-01-25 20:07:38 +0600

    remove punctuations

--- remove_punc_project/remove_punc_project/remove_punc_project/views.py
+++ remove_punc_project/remove_punc_project/remove_punc_project/views.py
@@ -9,14 +9,25 @@ def index(request):
 
 def analyze(request):
     djtext = request.GET.get('text', 'default')
+    
     removepunc = request.GET.get('removepunc', 'off')
+    fullcaps = request.GET.get('fullcaps', 'off')
+
     if removepunc == 'on':
-        punctuations = ''' !"#$%&'()*+, -./:;<=>?@[\]^_`{|}~'''
+        punctuations = '''!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~'''
         analyzed = ' '
         for char in djtext:
             if char not in punctuations:
                 analyzed = analyzed + char
         param = {'purpose': 'remove punctuations', 'analyzed_text': analyzed}
         return render(request, 'analyze.html', param)
+    
+    elif(fullcaps == 'on'):
+        analyzed = ' '
+        for char in djtext:
+            analyzed = analyzed + char.upper()
+        param = {'purpose': 'Change To UPPERCASE', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', param)
+        
     else:
         return HttpResponse("Errors")
